933077b988ae3a2eaf9bc0f3a60abc85104e3ebd
Zakladnyi Oleg <zakladniy@gmail.com>
2020-03-17 17:08:01 +0200

    Updates

---
+++ precache-manifest.165331f7a950b0e67881cc571e4f2fa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8ade5567399aed69a74881472f9832",
+    "url": "/reactwarriors-workshop/index.html"
+  },
+  {
+    "revision": "06e2ecba433d7690d3ac",
+    "url": "/reactwarriors-workshop/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9013caf20d99ccbf839f",
+    "url": "/reactwarriors-workshop/static/css/main.cab8b1b3.chunk.css"
+  },
+  {
+    "revision": "06e2ecba433d7690d3ac",
+    "url": "/reactwarriors-workshop/static/js/2.97862457.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactwarriors-workshop/static/js/2.97862457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9013caf20d99ccbf839f",
+    "url": "/reactwarriors-workshop/static/js/main.040bc919.chunk.js"
+  },
+  {
+    "revision": "d7e25a7be26e22c2c944",
+    "url": "/reactwarriors-workshop/static/js/runtime-main.2a191a5d.js"
+  }
+]);
